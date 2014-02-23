f037ca0f66555e5dbf247b4cd1e1021d812581a8
Chris Given <cmgiven@gmail.com>
2014-02-22 23:11:34 -0500

    Updates the data format to include both directions

--- process.rb
+++ process.rb
@@ -13,10 +13,16 @@ CSV.foreach('odd2014-feeder-pattern-enrollment-sy1112.csv', :headers => true, :h
 end
 
 school_codes = data.map { |r| r[:school_code] }
+school_codes = school_codes + data.map { |r| r[:new_school_code] }
+school_codes.reject! { |code| code == '' }
 school_codes.uniq!.sort!
 
 school_codes.each do |code|
-	schools[code] = {}
+	schools[code] = { :in => {}, :out => {} }
+end
+
+school_codes.each do |code|
+
 	rows = data.select { |r| r[:school_code] == code }
 	rows.each do |r|
 		r[:count] = r[:count] == -1 ? 5 : r[:count]
@@ -25,19 +31,44 @@ school_codes.each do |code|
 		when code
 			# Do nothing.
 		when ''
-			if !schools[code]['Other'] then schools[code]['Other'] = 0 end
-			schools[code]['Other'] = schools[code]['Other'] + r[:count]
+			# Do nothing.
 		else
-			if !schools[code][r[:new_school_code]] then schools[code][r[:new_school_code]] = 0 end
-			schools[code][r[:new_school_code]] = schools[code][r[:new_school_code]] + r[:count]
+			if !schools[code][:out][r[:new_school_code]] then
+				schools[code][:out][r[:new_school_code]] = 0
+			end
+			if !schools[r[:new_school_code]][:in][code] then
+				schools[r[:new_school_code]][:in][code] = 0
+			end
+			schools[code][:out][r[:new_school_code]] =
+				schools[code][:out][r[:new_school_code]] + r[:count]
+
+			schools[r[:new_school_code]][:in][code] =
+				schools[r[:new_school_code]][:in][code] + r[:count]
 		end
+	end
+end
+
+def clean (c)
+	a = []
+	c.each do |k, v|
+		h = {
+			:code => k.to_s,
+			:n => v
+		}
+
+		if h[:n] == 5 then
+			h[:n] = -1
+		end
+
+		a.push(h)
 	end
 
-	schools[code].each do |k, v|
-		if v == 5 then
-			schools[code][k] = -1
-		end
-	end
+	return a
+end
+
+schools.each do |k, v|
+	v[:in] = clean(v[:in])
+	v[:out] = clean(v[:out])
 end
 
 File.open(OUTPUT_FILE,"w") do |f|
